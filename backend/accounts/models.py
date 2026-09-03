342ba035eeeee3a2b469599986dc4ef3f2d4346d
Abhiram <abhi172993@gmail.com>
2026-09-03 09:10:12 +0530

    CustomUser model create

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        BUYER = "BUYER", "Buyer"
+        SELLER = "SELLER", "Seller"
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.BUYER
+    )
+    phone_number=models.CharField(max_length=15,blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"] #Will be useful when creating a superuser
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
+
+    @property
+    def is_seller(self):
+        return self.role == self.Role.SELLER
--- backend/plantproject/settings.py
+++ backend/plantproject/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'reviews'
 ]
 
+AUTH_USER_MODEL = "accounts.CustomUser"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
