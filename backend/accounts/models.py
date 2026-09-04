aeca4ee2c70c945410fcea21e43f1e5686ba1e40
Abhiram <abhi172993@gmail.com>
2026-09-04 20:34:57 +0530

    Seller model created

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class CustomUser(AbstractUser):
     class Role(models.TextChoices):
@@ -26,3 +27,43 @@ class CustomUser(AbstractUser):
     @property
     def is_seller(self):
         return self.role == self.Role.SELLER
+
+
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name = "seller_profile"
+    )
+
+    store_name = models.CharField(max_length=150)
+    bio=models.TextField(blank=True)
+    address = models.CharField(max_length=280)
+    city = models.CharField(max_length=100, db_index=True)
+
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90.0),MaxValueValidator(90.0)]
+    )
+    longitude = models.DecimalField(
+            max_digits=9,
+            decimal_places=6,
+            validators=[MinValueValidator(-180.0),MaxValueValidator(180.0)]
+    )
+
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["latitude", "longitude"])
+        ]
+
+    def __str__(self):
+        return self.store_name
+
+    @property
+    def google_maps_embed_url(self):
+        return f"https://maps.google.com/maps?q={self.latitude},{self.longitude}&z=15&output=embed"
