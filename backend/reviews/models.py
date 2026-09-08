6231db0d3713c9344b8581f0057861b69a2ff2ac
Abhiram <abhi172993@gmail.com>
2026-09-08 07:56:47 +0530

    create reviews model

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+from plants.models import Plant
 
-# Create your models here.
+
+class Review(models.Model):
+    plant = models.ForeignKey(
+        Plant, 
+        on_delete=models.CASCADE, 
+        related_name="reviews"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="reviews"
+    )
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        help_text="Rate 1 to 5 stars"
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        constraints = [
+            
+            models.UniqueConstraint(fields=["plant", "user"], name="unique_user_plant_review")
+        ]
+
+    def __str__(self):
+        return f"{self.rating}★ by {self.user.username} on {self.plant.name}"
